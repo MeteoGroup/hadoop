9a1de11a46d6e2734d8ec31e20d4bf74c6aa6282
Eric Karge <eric.karge@meteogroup.com>
2016-04-05 17:10:49 +0200

    Image tag can be passed as first argument of `build.sh` script.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --pull ${APACHE_MIRROR:+--build-arg "APACHE_MIRROR=$APACHE_MIRROR"} -t "meteogroup/hadoop:$(date -u '+%Y%m%dT%H%M%SZ')" .
+docker build --pull ${APACHE_MIRROR:+--build-arg "APACHE_MIRROR=$APACHE_MIRROR"} -t "${1:-meteogroup/hadoop}" .
